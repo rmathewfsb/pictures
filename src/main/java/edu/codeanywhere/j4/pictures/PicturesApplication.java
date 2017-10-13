542ecee3aa499a35364932a56a57fab608375c40
rmathewfsb <roy.mathew@flagstar.com>
2017-10-13 11:10:18 -0400

    Intial commit to master

---
+++ src/main/java/edu/codeanywhere/j4/pictures/PicturesApplication.java
@@ -0,0 +1,12 @@
+package edu.codeanywhere.j4.pictures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicturesApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/codeanywhere/j4/pictures/PicturesApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.codeanywhere.j4.pictures;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PicturesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
