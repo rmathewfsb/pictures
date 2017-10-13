c80cbee1ef1860c27c8823111c6463af651d2b1a
rmathewfsb <roy.mathew@flagstar.com>
2017-10-13 12:51:26 -0400

    Added basic auth using in-memory authentication manager.

---
+++ src/main/java/edu/codeanywhere/j4/pictures/WebAppSecurityConfig.java
@@ -0,0 +1,53 @@
+/*
+ *    Copyright &copy; Flagstar Bank 2017.
+ *
+ *    Copyright in the application source code, and in the information and
+ *    material therein and in their arrangement, is owned by Flagstar Bank, FSB
+ *    unless otherwise indicated.
+ *
+ *    You shall not remove or obscure any copyright, trademark or other notices.
+ *    You may not copy, republish, redistribute, transmit, participate in the
+ *    transmission of, create derivatives of, alter edit or exploit in any
+ *    manner any material including by storage on retrieval systems, without the
+ *    express written permission of Flagstar Bank.
+ */
+package edu.codeanywhere.j4.pictures;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * WebAppSecurityConfig.
+ *
+ * @author rmathew
+ */
+@Configuration
+@EnableWebSecurity
+public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter
+{
+    
+    @Value("${sec.authorized.role}")
+    private String authorizedRole;
+    @Override
+    protected void configure(final HttpSecurity httpSecurity) throws Exception
+    {
+        httpSecurity.authorizeRequests().antMatchers("/anonymous/**").permitAll()
+        .anyRequest().authenticated()
+        .and().httpBasic()
+        .and()
+        .csrf().disable();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("j4user").password("jjjj").roles("USER");
+    }
+}
+
